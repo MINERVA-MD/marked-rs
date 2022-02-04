0d232c732f062801b2d8ab43c8dbba9bad4ba12c
Jamie Samuel <jsam07@hotmail.com>
2022-02-04 01:36:23 -0800

    Add Test Suite for CommonMark

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,46 +1,54 @@
-use std::fs;
-use std::env;
-use std::path::Path;
-use serde_json::Result;
-use serde::{Serialize, Deserialize};
+macro_rules! spec_test {
+    ($path:expr, $from:expr, $to:expr) => {
+
+        #[cfg(test)]
+        mod tests {
+            use std::fs;
+            use std::env;
+            use seq_macro::seq;
+            use std::path::Path;
+            use serde_json::Result;
+            use test_case::test_case;
+            use serde::{Serialize, Deserialize};
 
 
-use md4rs_src::parser::parse::Parser;
+            use md4rs_src::parser::parse::Parser;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Spec {
-    markdown: String,
-    html: String,
-    example: i64,
-    start_line: i64,
-    end_line: i64,
-    section: String,
-}
+            #[derive(Serialize, Deserialize, Debug)]
+            struct Spec {
+                markdown: String,
+                html: String,
+                example: i64,
+                start_line: i64,
+                end_line: i64,
+                section: String,
+            }
 
-fn deserialize_specs(path: &str)-> String {
-    let data: String = fs::read_to_string(path)
-                            .expect("Unable to read file");
-    return data;
-}
+            fn deserialize_specs(path: &str)-> String {
+                let data: String = fs::read_to_string(path)
+                    .expect("Unable to read file");
+                return data;
+            }
 
-fn get_specs() -> Vec<Spec> {
-    let data: String = deserialize_specs("tests/fixtures/commonmark/spec-v3.json");
-    let specs: Vec<Spec> = serde_json::from_str(&data).unwrap();
-    return specs;
-}
+            fn get_specs() -> Vec<Spec> {
+                let data: String = deserialize_specs($path);
+                let specs: Vec<Spec> = serde_json::from_str(&data).unwrap();
+                return specs;
+            }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn run_specs() {
-        let specs: Vec<Spec> = get_specs();
-        for spec in specs.iter() {
-            let parse_actual = Parser::parse_markdown(&spec.markdown);
-            let parse_expected = &spec.html;
-            assert_eq!(parse_expected, parse_expected);
+            seq!(N in $from..$to {
+                #(#[test_case(N)])*
+                fn verify_specs(index: usize) {
+                    let specs: Vec<Spec> = get_specs();
+                    let spec: &Spec = &specs[index];
+                    let parse_actual = Parser::parse_markdown(&spec.markdown);
+                    let parse_expected = &spec.html;
+                    assert_eq!(*parse_expected, parse_actual);
+                }
+            });
         }
-    }
+    };
 }
 
+spec_test!("tests/fixtures/commonmark/spec-v3.json", 0, 651);
+// spec_test!("tests/fixtures/gfm/spec-v3.json", 0, 651);
