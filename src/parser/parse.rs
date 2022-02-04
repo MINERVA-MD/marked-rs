927df4aee110e3d8319c6a3269071260eeae20f2
Jamie Samuel <jsam07@hotmail.com>
2022-02-03 23:11:31 -0800

    🚀 Initial Commit

---
+++ src/parser/parse.rs
@@ -0,0 +1,14 @@
+pub struct Parser {
+    html: String,
+}
+
+impl Parser {
+    pub fn parse(markdown: &str)-> String {
+        return String::from(markdown);
+    }
+
+    pub fn parse_markdown(markdown: &str)-> String {
+        return String::from(markdown);
+    }
+}
+
---
+++ tests/tests.rs
@@ -0,0 +1,44 @@
+use std::fs;
+use std::env;
+use std::path::Path;
+use serde_json::Result;
+use serde::{Serialize, Deserialize};
+
+
+use md4rs_src::parser::parse::Parser;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Spec {
+    markdown: String,
+    html: String,
+    example: i64,
+    start_line: i64,
+    end_line: i64,
+    section: String,
+}
+
+fn deserialize_specs(path: &str)-> String {
+    let data: String = fs::read_to_string(path)
+                            .expect("Unable to read file");
+    return data;
+}
+
+fn get_specs() -> Vec<Spec> {
+    let data: String = deserialize_specs("tests/fixtures/commonmark/spec-v3.json");
+    let specs: Vec<Spec> = serde_json::from_str(&data).unwrap();
+    return specs;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run_specs() {
+        let specs: Vec<Spec> = get_specs();
+        for spec in specs.iter() {
+            // assert_eq!()
+        }
+    }
+}
+
