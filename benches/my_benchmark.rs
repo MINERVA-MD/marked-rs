0de5748c4cfc31fe584656e285afc704ac5bfc00
Jamie Samuel <jsam07@hotmail.com>
2022-03-26 09:17:36 -0700

    Drop release flag for testing

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -12,12 +12,13 @@ fn bench(c: &mut Criterion) {
     let mut marked = Marked::new(None);
     let mut lexer = Lexer::new(get_default_options());
     let md = fs::read_to_string("tests/fixtures/md/spec.md").expect("Unable to read file");
+    let md_sm = fs::read_to_string("tests/fixtures/md/spec-2.md").expect("Unable to read file");
 
 
 
-    c.bench_function("Pulldown Cmark", |b| b.iter(|| html::push_html(black_box(&mut String::new()), black_box(Parser::new_ext(md.as_str(), Options::empty())))));
-    c.bench_function("Comrak", |b| b.iter(|| markdown_to_html(black_box(md.as_str()), black_box(&ComrakOptions::default()))));
-    c.bench_function("Marked", |b| b.iter(|| marked.parse(black_box(md.as_str()), black_box(None), black_box(None))));
+    // c.bench_function("Pulldown Cmark", |b| b.iter(|| html::push_html(black_box(&mut String::new()), black_box(Parser::new_ext(md.as_str(), Options::empty())))));
+    // c.bench_function("Comrak", |b| b.iter(|| markdown_to_html(black_box(md.as_str()), black_box(&ComrakOptions::default()))));
+    c.bench_function("Marked", |b| b.iter(|| marked.parse(black_box(md_sm.as_str()), black_box(None), black_box(None))));
 }
 
 
